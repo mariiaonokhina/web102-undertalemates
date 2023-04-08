871d237bd512f48c975ab64b5c8a5dcfba6ae82c
mariiaonokhina <83292730+mariiaonokhina@users.noreply.github.com>
2023-04-08 10:37:57 -0400

    Added+style home page

--- undertalemates/src/App.jsx
+++ undertalemates/src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      Hello
+      <h1>Welcome to the Undertale-<i>mate</i> creator!</h1>
+      <h2>Here is where you can create your very own set of undertale-<i>mates</i>!</h2>
+      <img src='all-chars.jpeg' className='all-chars-img'></img>
     </div>
   )
 }
